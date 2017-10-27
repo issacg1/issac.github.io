e94be2b800f4c732af1a9e6b78ce168db1cbb4fc
Issac Grunwald <yitzygrunwald@gmail.com>
2017-10-27 12:29:06 -0400

    added projects

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+function openNewWindow(url){
+	window.open(url, "_blank")
+}
+
 $(document).ready(function(){
 		console.log("up and running!");
 		
@@ -5,18 +9,20 @@ $(document).ready(function(){
 		
 			var scrollVal = $(document).scrollTop().valueOf();
 			console.log(scrollVal)
-			if(scrollVal < 1000){	
+			if(scrollVal < 1500){	
 				$(".sideLeft").css({
 					'position' : 'sticky',
-					'height' : '100vh'
+					'height' : '100vh',
 				})
-			}else if(scrollVal > 1000){
+			}else if(scrollVal > 1500){
 				let height = $(".sideRight").css('height')
 				$(".sideLeft").css({
 					'position' : 'relative',
 					'height' : height,
 				})
 			}
+
+			$(".projectLeftSide").css("top",Math.max(0,2199-$(this).scrollTop()));
 		});
-		
+
 }); //End of doc. Don't modify above!
