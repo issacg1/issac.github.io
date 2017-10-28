9c9f4eff80cff0d887bc2749c84326efc79bee61
Issac Grunwald <yitzygrunwald@gmail.com>
2017-10-28 19:11:17 -0400

    added contact section, added anchors to projects

--- js/main.js
+++ js/main.js
@@ -2,18 +2,24 @@ function openNewWindow(url){
 	window.open(url, "_blank")
 }
 
+
+
+
 $(document).ready(function(){
 		console.log("up and running!");
 		
+
+		// add $(window).width(); as a conditional for responsivness
 		$(window).scroll(function (event) {
 		
 			var scrollVal = $(document).scrollTop().valueOf();
 			console.log(scrollVal)
 			if(scrollVal < 1500){	
 				$(".sideLeft").css({
-					'position' : 'sticky',
-					'height' : '100vh',
+					'position' : 'fixed',
+					'height' : '100vh'
 				})
+				$(".sideLeft").css("top", Math.max(0, 689 - $(this).scrollTop()));
 			}else if(scrollVal > 1500){
 				let height = $(".sideRight").css('height')
 				$(".sideLeft").css({
@@ -21,8 +27,20 @@ $(document).ready(function(){
 					'height' : height,
 				})
 			}
-
-			$(".projectLeftSide").css("top",Math.max(0,2199-$(this).scrollTop()));
+			
+			if(scrollVal < 4180){
+				$(".projectLeftSide").css({
+					'position' : 'fixed',
+					'height' : '100vh'
+				})
+				$(".projectLeftSide").css("top",Math.max(0,2199-$(this).scrollTop()));
+			} else if (scrollVal > 4180){
+				let height1 = $(".projectRightSide").css('height')
+				$(".projectLeftSide").css({
+					'position' : 'relative',
+					'height' : height1,
+				})
+			}
 		});
 
 }); //End of doc. Don't modify above!
