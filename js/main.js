4fd9a413b1337d41e7919efe2967ffa70684c928
Issac Grunwald <yitzygrunwald@gmail.com>
2017-11-01 13:31:13 -0400

    additional copy

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 function openNewWindow(url){
 	window.open(url, "_blank")
-}
+};
 
 
 $(document).ready(function(){
@@ -10,38 +10,38 @@ $(document).ready(function(){
 
 		var width = $(window).width();
 
-		$(window).scroll(function (event) {
+	$(window).scroll(function (event) {
+		var scrollVal = $(document).scrollTop().valueOf();
+		width = $(window).width();
+		console.log(scrollVal);
+
+		if(scrollVal < 1500 && width > 810){	
+			$(".sideLeft").css({
+				'position' : 'fixed',
+				'height' : '100vh'
+			});
+			$(".sideLeft").css("top", Math.max(0, 689 - $(this).scrollTop()));
+		}else if(scrollVal > 1500 && width > 810){
+			var height = $(".sideRight").css('height');
+			$(".sideLeft").css({
+				'position' : 'relative',
+				'height' : height,
+			});
+		};
 		
-			var scrollVal = $(document).scrollTop().valueOf();
-			width = $(window).width();
-			console.log(width)
-			if(scrollVal < 1500 && width > 810){	
-				$(".sideLeft").css({
-					'position' : 'fixed',
-					'height' : '100vh'
-				})
-				$(".sideLeft").css("top", Math.max(0, 689 - $(this).scrollTop()));
-			}else if(scrollVal > 1500 && width > 810){
-				let height = $(".sideRight").css('height')
-				$(".sideLeft").css({
-					'position' : 'relative',
-					'height' : height,
-				})
-			}
-			
-			if(scrollVal < 4180 && width > 810){
-				$(".projectLeftSide").css({
-					'position' : 'fixed',
-					'height' : '100vh'
-				})
-				$(".projectLeftSide").css("top",Math.max(0,2199-$(this).scrollTop()));
-			} else if (scrollVal > 4180 && width > 810){
-				let height1 = $(".projectRightSide").css('height')
-				$(".projectLeftSide").css({
-					'position' : 'relative',
-					'height' : height1,
-				})
-			}
-		});
+		if(scrollVal < 4180 && width > 810){
+			$(".projectLeftSide").css({
+				'position' : 'fixed',
+				'height' : '100vh'
+			});
+			$(".projectLeftSide").css("top",Math.max(0,2199-$(this).scrollTop()));
+		} else if (scrollVal > 4180 && width > 810){
+			let height1 = $(".projectRightSide").css('height')
+			$(".projectLeftSide").css({
+				'position' : 'relative',
+				'height' : height1,
+			});
+		};
+	});
 
 }); //End of doc. Don't modify above!
